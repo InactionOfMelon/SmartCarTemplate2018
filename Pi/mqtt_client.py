bb75946c3918e82a0c93b0b29bcba32da7717958
Manchery <manchery@outlook.com>
2018-11-08 23:05:19 +0800

    Update Pi/mqtt_client.py

--- Pi/mqtt_client.py
+++ Pi/mqtt_client.py
@@ -13,30 +13,34 @@ def mqtt_on_connect(client, userdata, flags, rc):
 def mqtt_on_message(client,	userdata, msg):
 	if msg.topic == env.MQTT_TOPIC_RCCONTROL:
 		if msg.payload[0:7] == 'forward':
-			car.forward(__str2int(msg.payload[7:], env.CAR_DEFAULT_SPEED))
+                        if msg.payload[7:] == '':
+                                car.forward()
+                        else:
+			        car.forward_param(__str2int(msg.payload[7:], env.CAR_DEFAULT_SPEED))
 		elif msg.payload == 'stop':
 			car.stop()
 		elif msg.payload[0:8] == 'backward':
-			car.backward(__str2int(msg.payload[8:], env.CAR_DEFAULT_SPEED))
+                        if msg.payload[8:] == '':
+                                car.backward()
+                        else:
+			        car.backward_param(__str2int(msg.payload[8:], env.CAR_DEFAULT_SPEED))
 		elif msg.payload[0:4] == 'left':
-			car.left(__str2int(msg.payload[4:], env.CAR_DEFAULT_SPEED_DIFF))
+                        if msg.payload[4:] == '':
+                                car.left()
+                        else:
+                                car.left_param(__str2int(msg.payload[4:], env.CAR_DEFAULT_SPEED_DIFF))
 		elif msg.payload[0:5] == 'right':
-			car.right(__str2int(msg.payload[5:], env.CAR_DEFAULT_SPEED_DIFF))
+                        if msg.payload[5:] == '':
+                                car.right()
+                        else:
+			        car.right_param(__str2int(msg.payload[5:], env.CAR_DEFAULT_SPEED_DIFF))
 		elif msg.payload == 'clockwise':
 			car.clockwise()
 		elif msg.payload == 'anti-clockwise':
 			car.anticlockwise()
 		elif msg.payload[0:5] == 'other':
 			pass
-			#forward()
-			#forward_param(uint16_t)
-			#backward()
-			#backward_param(uint16_t)
-			#left
-			#left_param(uint16_t)
-			#right
-			#right_param(uint16_t)
-
+			
 	print("%s: %s" % (msg.topic, msg.payload))
 
 client = mqtt.Client()
