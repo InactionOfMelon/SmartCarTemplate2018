44824da64268e58f8ef8e5dd503df6a0fc5dd235
blutrex <blutrex@gmail.com>
2018-11-09 10:25:39 +0800

    space to tab

--- Pi/mqtt_client.py
+++ Pi/mqtt_client.py
@@ -10,37 +10,36 @@ def mqtt_on_connect(client, userdata, flags, rc):
 	client.subscribe(env.MQTT_TOPIC_STATUS)
 	client.publish(env.MQTT_TOPIC_STATUS, env.MQTT_CONNECT_ERROR[rc])
 
-def mqtt_on_message(client,	userdata, msg):
+def mqtt_on_message(client, userdata, msg):
 	if msg.topic == env.MQTT_TOPIC_RCCONTROL:
 		if msg.payload[0:7] == 'forward':
-                        if msg.payload[7:] == '':
-                                car.forward()
-                        else:
-			        car.forward_param(__str2int(msg.payload[7:], env.CAR_DEFAULT_SPEED))
+			if msg.payload[7:] == '':
+				car.forward()
+			else:
+				car.forward_param(__str2int(msg.payload[7:], env.CAR_DEFAULT_SPEED))
 		elif msg.payload == 'stop':
 			car.stop()
 		elif msg.payload[0:8] == 'backward':
-                        if msg.payload[8:] == '':
-                                car.backward()
-                        else:
-			        car.backward_param(__str2int(msg.payload[8:], env.CAR_DEFAULT_SPEED))
+			if msg.payload[8:] == '':
+				car.backward()
+			else:
+				car.backward_param(__str2int(msg.payload[8:], env.CAR_DEFAULT_SPEED))
 		elif msg.payload[0:4] == 'left':
-                        if msg.payload[4:] == '':
-                                car.left()
-                        else:
-                                car.left_param(__str2int(msg.payload[4:], env.CAR_DEFAULT_SPEED_DIFF))
+			if msg.payload[4:] == '':
+				car.left()
+			else:
+				car.left_param(__str2int(msg.payload[4:], env.CAR_DEFAULT_SPEED_DIFF))
 		elif msg.payload[0:5] == 'right':
-                        if msg.payload[5:] == '':
-                                car.right()
-                        else:
-			        car.right_param(__str2int(msg.payload[5:], env.CAR_DEFAULT_SPEED_DIFF))
+			if msg.payload[5:] == '':
+				car.right()
+			else:
+				car.right_param(__str2int(msg.payload[5:], env.CAR_DEFAULT_SPEED_DIFF))
 		elif msg.payload == 'clockwise':
 			car.clockwise()
 		elif msg.payload == 'anti-clockwise':
 			car.anticlockwise()
 		elif msg.payload[0:5] == 'other':
 			pass
-		
 	print("%s: %s" % (msg.topic, msg.payload))
 
 client = mqtt.Client()
