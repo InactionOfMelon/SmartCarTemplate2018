dedd762ff8ec883605ace69095e9f3e014fe1bf7
Manchery <manchery@outlook.com>
2018-12-11 15:55:16 +0800

    ready for pid test

--- laneLineDetect/camera.py
+++ laneLineDetect/camera.py
@@ -3,15 +3,15 @@ import time
 import detect
 import numpy as np
 
-cap=cv2.VideoCapture(0)
-print "camera launched"
+#cap=cv2.VideoCapture(0)
+#print "camera launched"
 
 def dist(x,y,rho,theta):
     a,b = np.cos(theta),np.sin(theta)
     x0,y0 = a*rho,b*rho
     x1,y1 = (x0 + 100*(-b)),(y0 + 100*(a))
     x2,y2 = (x0 - 100*(-b)),(y0 - 100*(a))
-    print x1,y1,x2,y2
+    #print x1,y1,x2,y2
     array_longi  = np.array([x2-x1, y2-y1])
     array_trans = np.array([x-x1, y-y1])
     array_temp = (float(array_trans.dot(array_longi)) / array_longi.dot(array_longi))
@@ -21,6 +21,8 @@ def dist(x,y,rho,theta):
 
 while True:
     ret,frame=cap.read()
+    #frame=cv2.imread('5.jpg')
+    #ret=True
     h,w=frame.shape[:2]
     if not ret:
         print "capture error"
@@ -38,6 +40,9 @@ while True:
 
     distLeft=dist(w/2,h,lines[0,0,0],lines[0,0,1])
     distRight=dist(w/2,h,lines[1,0,0],lines[1,0,1])
-    
+    print distLeft,distRight
+
+    #break
+
     #time.sleep(1)
 
